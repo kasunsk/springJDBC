ab61eb887db15970abfa45915709eb5b7b45d890
Kasun Kariyawasam <kkariyawasam@isa.ae>
2014-02-07 15:51:18 +0530

    sql file was added

---
+++ src/main/resources/vilage.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE vilage;
+
+USE vilage;
+
+CREATE TABLE persion(Name varchar(40) not NULL, ID varchar(12) not NULL PRIMARY KEY,Sex varchar(6) not NULL, Address varchar(60) not NULL, TPNum varchar(15) not NULL);
