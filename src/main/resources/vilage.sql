34fa11239fdd949ae399da976783962dd8abc577
Kasun Kariyawasam <kkariyawasam@isa.ae>
2014-02-11 11:52:03 +0530

    added a column to standered database

--- src/main/resources/vilage.sql
+++ src/main/resources/vilage.sql
@@ -2,4 +2,4 @@ CREATE DATABASE vilage;
 
 USE vilage;
 
-CREATE TABLE persion(Name varchar(40) not NULL, ID varchar(12) not NULL PRIMARY KEY,Sex varchar(6) not NULL, Address varchar(60) not NULL, TPNum varchar(15) not NULL);
+CREATE TABLE persion(Name varchar(40) not NULL, ID varchar(12) not NULL PRIMARY KEY,Sex varchar(6) not NULL, Address varchar(60) not NULL, TPNum varchar(15) not NULL, Birth_Date varchar(25));
